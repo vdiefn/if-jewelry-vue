89f991f7727035780ef75034409c5a9bea61d67b
Ellen <ddhkxxhxx@hotmail.com>
2025-05-04 21:50:40 +0800

    feat:add eslint

--- eslint.config.js
+++ eslint.config.js
@@ -5,16 +5,23 @@ import pluginVue from "eslint-plugin-vue";
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,vue}"] },
-  { files: ["**/*.{js,mjs,cjs,vue}"], languageOptions: { globals: globals.browser } },
-  { files: ["**/*.{js,mjs,cjs,vue}"], plugins: { js }, extends: ["js/recommended"] },
+  {
+    files: ["**/*.{js,mjs,cjs,vue}"],
+    languageOptions: { globals: globals.browser },
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,vue}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
   pluginVue.configs["flat/essential"],
   {
-        rules: {
-            'no-var': 'error',
-            'no-multiple-empty-lines': ['warn', { max: 1 }],
-            'no-unexpected-multiline': 'error',
-            "prettier/prettier": "warn",
-          
-        }
-    }
+    rules: {
+      "no-var": "error",
+      "no-multiple-empty-lines": ["warn", { max: 1 }],
+      "no-unexpected-multiline": "error",
+      "prettier/prettier": "warn",
+      "space-in-parens": ["warn", "always"],
+    },
+  },
 ]);
