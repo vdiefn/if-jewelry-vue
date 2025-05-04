e973ff27ca14b9c10d2ad30c0ec0582f3a3b5266
Ellen <ddhkxxhxx@hotmail.com>
2025-05-04 22:15:43 +0800

    feat: modified interceptors

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-import { ElMessage } from "element-plus"
+import { useUserStore } from "@/store/modules/user"
 
 const request = axios.create({
   baseURL: import.meta.env.VITE_APP_API_URL,
@@ -7,38 +7,18 @@ const request = axios.create({
 })
 
 request.interceptors.request.use((config) => {
+  const userStore = useUserStore()
+  if(userStore.token){
+    config.headers.Authorization = userStore.token
+  }
   return config
 })
 
-
-request.interceptors.response.use((response) => {
-  return response.data
-
-}, (error) => {
-  let message = ""
-  let status = error.response.status
-  switch(status) {
-    case 401:
-            message = "token過期";
-            break;
-        case 403:
-            message = '無權訪問';
-            break;
-        case 404:
-            message = "請求地址錯誤";
-            break;
-        case 500:
-            message = "伺服器出現問題";
-            break;
-        default:
-            message = "網路異常";
+request.interceptors.response.use(
+  response => response.data, 
+  error => {
+    return Promise.reject(error) 
   }
-
-  ElMessage({
-    type:error,
-    message
-  })
-  return Promise.reject(error)
-})
+)
 
 export default request
