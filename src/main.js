4415d9ec983dbf28e19110b21650dd758eb394c2
Ellen <ddhkxxhxx@hotmail.com>
2025-05-15 22:25:58 +0800

    feat: add fontawesome

--- src/main.js
+++ src/main.js
@@ -5,20 +5,26 @@ import App from './App.vue'
 import 'element-plus/dist/index.css'
 import ElementPlus from 'element-plus'
 
+/* fontawesome */
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+import { faCheck, faXmark } from '@fortawesome/free-solid-svg-icons'
+import {} from '@fortawesome/free-solid-svg-icons'
+import {} from "@fortawesome/free-regular-svg-icons";
+import {} from "@fortawesome/free-brands-svg-icons";
+
+library.add(faCheck, faXmark)
 
 import router from "./router"
 
 import pinia from './store'
 
-import {  } from '@fortawesome/free-solid-svg-icons'
-import {} from "@fortawesome/free-regular-svg-icons";
-import {} from "@fortawesome/free-brands-svg-icons";
+
 
 
 const app = createApp(App)
-library.add()
+
 
 app.component('font-awesome-icon', FontAwesomeIcon)
 app.use(ElementPlus)
