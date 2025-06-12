5ebc8c494ac85cf2a825bfd8855ecba35f6e6c33
Ellen <ddhkxxhxx@hotmail.com>
2025-06-12 22:22:38 +0800

    feat: Add frontend views page"

---
+++ src/views/front/Home.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { ref } from 'vue';
+
+</script>
+
+<template>
+    <h3>Home</h3>
+</template>
+
+<style scoped>
+</style>
---
+++ src/views/front/ProductDetail.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { ref } from 'vue';
+
+</script>
+
+<template>
+    <h3>Product Detail</h3>
+</template>
+
+<style scoped>
+</style>
