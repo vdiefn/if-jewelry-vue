f33dc10773063a6755c4c14991e7b87abc85ee6c
Ellen <ddhkxxhxx@hotmail.com>
2025-06-12 22:23:51 +0800

    feat: Modified navigation guard

--- src/permission.js
+++ src/permission.js
@@ -8,25 +8,28 @@ const userStore = useUserStore(pinia)
 let isUserChecked = false
 
 router.beforeEach(async(to, from) => {
-  const token = userStore.token
-  document.title = `${setting.title}`
+    const token = userStore.token
+    document.title = `${setting.title}`
 
-  if(!token && to.name !== "login") {
-    return { name: "login"}
-  }
+    const isBackendRoute = to.path.startsWith("/admin")
 
-  if(token){
-    if(to.name === "login"){
-      return { path: "/" }
+    if(isBackendRoute && !token && to.name !== "login") {
+        return { name: "login"}
     }
-    if (!isUserChecked) {
-      try {
-        await userStore.userCheck()
-        isUserChecked = true
-      } catch(error) {
-        userStore.userLogout()
-        return { path: "/login" }
-      }
+
+    if(token && to.name === "login") {
+        return { name: "dashboard"}
     }
-  }
+
+    if(token && !isUserChecked && isBackendRoute) {
+        try {
+            await userStore.userCheck()
+            isUserChecked = true
+        } catch (error) {
+            userStore.userLogout()
+            return { name: "login" }
+        }
+    }
+
+    return true
 })
