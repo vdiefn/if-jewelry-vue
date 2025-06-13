a8db39d6fa6ea4e2074e31efd140dff112e5d724
ellen.kuo <ellen.kuo@patrioimemory.com>
2025-06-13 17:20:40 +0800

    refactor: Modified style setting
    1. 修改樣式設定

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
-import './styles/index.scss'
+import '@/assets/style/index.scss'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-import 'element-plus/dist/index.css'
+/* Element Plus */
 import ElementPlus from 'element-plus'
+import '@/assets/style/element/index.scss'
 
 /* fontawesome */
 import { library } from '@fortawesome/fontawesome-svg-core'
@@ -23,8 +24,8 @@ import pinia from './store'
 const app = createApp(App)
 
 app.component('font-awesome-icon', FontAwesomeIcon)
-app.use(ElementPlus)
 app.use(router)
 app.use(pinia)
+app.use(ElementPlus)
 import "./permission"
 app.mount('#app')
