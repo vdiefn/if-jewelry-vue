30a1bf83ade0cf91b98f6c9b153983b4530aead6
Ellen <ddhkxxhxx@hotmail.com>
2025-06-22 22:39:43 +0800

    feat: add loading function

--- src/views/front/Products.vue
+++ src/views/front/Products.vue
@@ -9,14 +9,18 @@ const data = ref({})
 const route = useRoute()
 const totalPage= ref(1)
 const currentPage = ref(1)
+const loading = ref(false)
 
 const getProducts = async (page, category) => {
+    loading.value = true
     try {
         data.value = await reqProducts(page, category);
         totalPage.value = data.value.pagination.total_pages
         currentPage.value = page
     } catch(error) {
         console.error(error);
+    } finally {
+        loading.value = false
     }
 }
 
@@ -29,30 +33,44 @@ watch(route, (value) => {
 })
 
 onMounted(() => {
-    getProducts()
+    getProducts(1, route.query.category)
 })
 
 </script>
 
 <template>
-    <div class="card-wrapper">
-        <CardProduct :data="product" v-for="product in data.products" :key="product.id"/>
-    </div>
-    <div class="pagination-block">
-        <ElPagination layout="prev, pager, next" :page-count="totalPage" @current-change="changeCurrentPage"/>
+    <div class="container" v-loading="loading">
+        <template v-if="route.query.category">
+            <h5>{{route.query.category}}</h5>
+        </template>
+        <template v-else>
+            <h5>全部商品</h5>
+        </template>
+        <div class="card-wrapper">
+            <CardProduct :data="product" v-for="product in data.products" :key="product.id"/>
+        </div>
+        <div class="pagination-block">
+            <ElPagination layout="prev, pager, next" :page-count="totalPage" @current-change="changeCurrentPage"/>
+        </div>
     </div>
 </template>
 
 <style scoped lang="scss">
-.card-wrapper {
-    display: grid;
-    grid-template-columns: repeat(2, minmax(150px, 1fr));
-    grid-gap: 1rem;
+.container {
     margin-top: $base-header-height-tablet;
-    padding: 1rem 1rem;
-    width: 100%;
-    justify-items: center;
-}
+
+    > h5 {
+        padding: 0.5rem 1rem;
+    }
+
+    .card-wrapper {
+        display: grid;
+        grid-template-columns: repeat(2, minmax(150px, 1fr));
+        grid-gap: 1rem;
+        padding: 1rem 1rem;
+        width: 100%;
+        justify-items: center;
+    }
 
 .pagination-block {
     display: flex;
@@ -66,17 +84,19 @@ onMounted(() => {
     }
 }
 
-@media(min-width: $breakpoint-tablet) {
-    .card-wrapper {
-        display: grid;
-        grid-template-columns: repeat(3, minmax(200px, 1fr));
+    @media(min-width: $breakpoint-tablet) {
+        .card-wrapper {
+            display: grid;
+            grid-template-columns: repeat(3, minmax(200px, 1fr));
+        }
+    }
+
+    @media(min-width: $breakpoint-desktop) {
+        .card-wrapper {
+            display: grid;
+            grid-template-columns: repeat(4, minmax(200px, 1fr));
+        }
     }
 }
 
-@media(min-width: $breakpoint-desktop) {
-    .card-wrapper {
-        display: grid;
-        grid-template-columns: repeat(4, minmax(200px, 1fr));
-    }
-}
 </style>
