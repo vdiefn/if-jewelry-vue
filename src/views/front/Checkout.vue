139d6e0aee0ecdaf7a4a44c796f2d809b3ee459c
ellen.kuo <ellen.kuo@patrioimemory.com>
2025-06-30 17:25:51 +0800

    feat(Checkout): Add checkout page layout

--- src/views/front/Checkout.vue
+++ src/views/front/Checkout.vue
@@ -10,7 +10,19 @@ const form = reactive({
     address:"",
     phone:"",
     email:"",
-    message:""
+    message:"",
+    delivery:""
+
+})
+const creditCardForm = reactive({
+    cardHolderName: "",
+    cardNumber1: "",
+    cardNumber2: "",
+    cardNumber3: "",
+    cardNumber4: "",
+    cvvNumber: "",
+    expireMonth:"",
+    expireYear: "",
 })
 
 const nextStep = () => {
@@ -70,22 +82,81 @@ const submitOrder = () => {
 
             <template v-else-if="activeStep === 1">
                 <ElCard>
-                    <h3>配送方式</h3>
-                    <ElRadioGroup v-model="form.delivery">
-                        <ElRadio label="宅配">宅配</ElRadio>
-                        <ElRadio label="超商取貨">超商取貨</ElRadio>
-                    </ElRadioGroup>
+                    <ElForm :model="form" label-width="auto">
+                        <ElFormItem label="寄送方式">
+                            <ElRadioGroup v-model="form.delivery">
+                                <ElRadio label="宅配" border>宅配</ElRadio>
+                                <ElRadio label="超商取貨不付款" border>超商取貨不付款</ElRadio>
+                            </ElRadioGroup>
+                        </ElFormItem>
+                    </ElForm>
                 </ElCard>
 
             </template>
 
             <template v-else-if="activeStep === 2">
                 <ElCard>
-                    <h3>付款資訊</h3>
-                    <ElRadioGroup v-model="form.payment">
-                        <ElRadio label="信用卡">信用卡</ElRadio>
-                        <ElRadio label="貨到付款">貨到付款</ElRadio>
-                    </ElRadioGroup>
+                    <ElForm label-width="100px">
+                        <ElFormItem label="付款方式">
+                            <ElRadioGroup v-model="form.payment">
+                                <ElRadio label="信用卡" value="credit card" border>信用卡</ElRadio>
+                                <ElRadio label="ATM轉帳" value="atm transfer" border>ATM轉帳</ElRadio>
+                            </ElRadioGroup>
+                        </ElFormItem>
+                        <template v-if="form.payment === 'credit card'">
+                            <ElForm :model="creditCardForm" label-width="100px">
+                                <ElFormItem label="持卡人姓名">
+                                    <ElInput v-model="creditCardForm.cardHolderName" placeholder="請輸入持卡人姓名" style="width: 30%;"/>
+                                </ElFormItem>
+                                <ElFormItem label="信用卡卡號">
+                                    <ElRow :gutter="10">
+                                        <ElCol :span="6">
+                                            <ElInput v-model="creditCardForm.cardNumber1" placeholder="1234" maxlength="4" />
+                                        </ElCol>
+                                        <ElCol :span="6">
+                                            <ElInput v-model="creditCardForm.cardNumber2" placeholder="5678" maxlength="4" />
+                                        </ElCol>
+                                        <ElCol :span="6">
+                                            <ElInput v-model="creditCardForm.cardNumber3" placeholder="9012" maxlength="4" />
+                                        </ElCol>
+                                        <ElCol :span="6">
+                                            <ElInput v-model="creditCardForm.cardNumber4" placeholder="3456" maxlength="4" />
+                                        </ElCol>
+                                    </ElRow>
+                                </ElFormItem>
+                                <ElFormItem label="到期年月">
+                                    <div style="display: flex; gap: 10px; width: 30%;">
+                                        <ElSelect
+                                            v-model="creditCardForm.expireMonth"
+                                            placeholder="月 (MM)"
+                                            style="flex: 1"
+                                        >
+                                            <ElOption v-for="month in months" :key="month" :label="month" :value="month" />
+                                        </ElSelect>
+
+                                        <ElSelect
+                                            v-model="creditCardForm.expireYear"
+                                            placeholder="年 (YYYY)"
+                                            style="flex: 1"
+                                        >
+                                            <ElOption v-for="year in years" :key="year" :label="year" :value="year" />
+                                        </ElSelect>
+                                    </div>
+                                </ElFormItem>
+                                <ElFormItem label="後三碼">
+                                    <ElInput v-model="creditCardForm.cvvNumber" placeholder="請輸入後三碼" maxlength="3" show-password style="width: 30%;"/>
+                                </ElFormItem>
+                            </ElForm>
+                        </template>
+                        <template v-else-if="form.payment === 'atm transfer'">
+                            <ElCard class="account-info">
+                                <h6>帳戶資訊</h6>
+                                <p>銀行名稱: OX銀行 (ox分行)</p>
+                                <p>銀行代碼: 999</p>
+                                <p>銀行帳號: 1234567654321</p>
+                            </ElCard>
+                        </template>
+                    </ElForm>
                 </ElCard>
             </template>
         </div>
@@ -128,6 +199,17 @@ const submitOrder = () => {
         }
     }
 
+    .step-actions {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+
+        .el-button {
+            margin: 5px 0;
+            width: 150px;
+        }
+    }
+
     .el-card {
         margin: 2rem auto;
 
@@ -135,6 +217,20 @@ const submitOrder = () => {
 
     .el-collapse {
         margin: 20px auto;
+    }
+}
+
+@media (min-width: $breakpoint-tablet) {
+    .container {
+        max-width: 750px;
+        width: 100%;
+    }
+}
+
+@media (min-width: $breakpoint-desktop) {
+    .container {
+        max-width: 1000px;
+        width: 100%;
     }
 }
 </style>
