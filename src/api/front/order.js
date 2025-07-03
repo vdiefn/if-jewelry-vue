4226875e08c3721b0485c97c3083802664995d97
ellen.kuo <ellen.kuo@patrioimemory.com>
2025-07-03 15:17:01 +0800

    feat(api): Add reqGetOrder

--- src/api/front/order.js
+++ src/api/front/order.js
@@ -3,3 +3,7 @@ import api from "@/utils/api.js";
 export const reqAddOrder = (payload) => {
     return api.post("/order", payload)
 }
+
+export const reqGetOrder = (id) => {
+    return api.get(`/order/${id}`)
+}
