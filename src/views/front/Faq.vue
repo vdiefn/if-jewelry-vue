8b5167c845b08eead7f17eb8847f2922a21ecffe
ellen.kuo <ellen.kuo@patrioimemory.com>
2025-07-04 15:25:52 +0800

    feat(Faq): Add component and update content

--- src/views/front/Faq.vue
+++ src/views/front/Faq.vue
@@ -1,59 +1,20 @@
 <script setup>
-import { ref } from 'vue'
-import { ElCollapse, ElCollapseItem } from "element-plus"
 import { faqs } from "@/data/faqs.js"
-
-const activeName = ref(null)
+import { QuestionAndAnswerForm } from "@/components/front/index.js";
 
 </script>
 
 <template>
 <div class="container">
     <h3>常見問題 FAQ</h3>
-    <ElCollapse v-model="activeName" accordion>
-        <ElCollapseItem :name="index" v-for="(item, index) in faqs" :key="index">
-            <template #title="{ isActive }">
-                <div :class="['title-wrapper', {'is-active': isActive}]">
-                    <h5>{{ item.question }}</h5>
-                </div>
-            </template>
-            <div>
-                {{ item.answer }}
-            </div>
-        </ElCollapseItem>
-    </ElCollapse>
-
+    <QuestionAndAnswerForm :data="faqs"/>
 </div>
 </template>
 
 <style scoped lang="scss">
 .container {
     padding: 2rem 1rem;
-    margin: 50px auto 20px auto;
-
-    .el-collapse {
-        border: 1px solid lightgray;
-        margin: 15px auto;
-        padding: 1rem;
-        background-color: $base-background-color;
-        border-radius: 5px;
-        --el-collapse-header-bg-color: $base-background-color;
-
-        :deep(.el-collapse-item__wrap) {
-            background-color: $base-background-color;
-        }
-    }
-
-    .title-wrapper {
-        display: flex;
-        align-items: center;
-        gap: 4px;
-    }
-
-    :deep(.el-collapse-item.is-active .title-wrapper) {
-        color: black;
-        font-weight: 900;
-    }
+    margin: 40px auto 0 auto;
 }
 
 @media(min-width: $breakpoint-tablet){
