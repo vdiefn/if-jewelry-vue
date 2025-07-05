915ca9c10f584e116daf6aa7b7b7c9c1c660bccc
Ellen <ddhkxxhxx@hotmail.com>
2025-07-05 22:10:51 +0800

    feat(DefaultContainer): Add component DefaultContainer

---
+++ src/components/front/DefaultContainer.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from 'vue';
+
+</script>
+
+<template>
+    <div class="container">
+        <slot></slot>
+    </div>
+</template>
+
+<style scoped lang="scss">
+.container {
+    margin-top: $base-header-height;
+}
+</style>
