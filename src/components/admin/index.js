6065ebc9045288b5cb5d9a8084ae7ef6429d65c2
ellen.kuo <ellen.kuo@patrioimemory.com>
2025-07-07 15:33:16 +0800

    feat: Add DialogAdminOrder.vue

--- src/components/admin/index.js
+++ src/components/admin/index.js
@@ -1,17 +1,3 @@
-import * as ElementPlusIconsVue from '@element-plus/icons-vue'
-
-export default {
-    install(app) {
-        Object.keys(components).forEach((key) => {
-        app.component(key, components[key])
-        })
-
-        //將element-plus提供的icon註冊為全局組件
-        for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-            app.component(key, component)
-            }
-    },
-}
-
 export { default as DialogAdminProduct } from "./DialogAdminProduct.vue"
 export { default as DialogAdminCoupon } from "./DialogAdminCoupon.vue"
+export { default as DialogAdminOrder } from "./DialogAdminOrder.vue"
