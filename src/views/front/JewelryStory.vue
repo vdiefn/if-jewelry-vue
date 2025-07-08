63950ccb2998dc2c2e0a6040d52e3770a6bc4870
ellen.kuo <ellen.kuo@patrioimemory.com>
2025-07-08 15:58:26 +0800

    feat(JewelryStory): Add DefaultContainer

--- src/views/front/JewelryStory.vue
+++ src/views/front/JewelryStory.vue
@@ -1,10 +1,21 @@
 <script setup>
 import { ref } from 'vue';
+import {DefaultContainer} from "@/components/front/index.js";
 
 </script>
 
 <template>
+    <DefaultContainer>
+        <div class="outer-container">
+            <h3>寶石小教室</h3>
+        </div>
+
+    </DefaultContainer>
 </template>
 
 <style scoped>
+.outer-container {
+    border: 1px solid red;
+    padding: 10px;
+}
 </style>
