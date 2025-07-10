8ee5a0021809b3cfb7968d17656858b6ef46f666
ellen.kuo <ellen.kuo@patrioimemory.com>
2025-07-10 14:48:22 +0800

    style(JewelryStory): Update css layout

--- src/views/front/JewelryStory.vue
+++ src/views/front/JewelryStory.vue
@@ -3,7 +3,7 @@ import { ref, onMounted } from 'vue';
 import { DefaultContainer } from "@/components/front/index.js";
 import { reqAllArticles } from "@/api/front/article.js"
 import { useRouter } from "vue-router";
-import { ElBacktop } from "element-plus";
+import { ElCard } from "element-plus";
 
 const data = ref()
 const router = useRouter();
@@ -38,17 +38,14 @@ onMounted(() => {
     <DefaultContainer>
         <div class="outer-container">
             <h3>寶石小教室</h3>
+            <ElCard v-for="item in data" :key="item.id" v-loading="loading" shadow="hover">
+                <div class="story-wrapper">
+                    <h5>{{ item.title }}</h5>
+                    <p>{{ item.description.slice(0, 50) }} ......</p>
+                    <span class="read-more" @click="goToArticle(item.id)">READ MORE</span>
+                </div>
+            </ElCard>
 
-            <div class="story-wrapper" v-for="item in data" :key="item.id" v-loading="loading">
-                <div class="img-wrapper">
-                    <img :src="item.image" alt="story picture" />
-                </div>
-                <div class="text-wrapper">
-                    <h5>{{item.title}}</h5>
-                    <p>{{item.description}}</p>
-                    <small class="read-more" @click="goToArticle(item.id)">READ MORE</small>
-                </div>
-            </div>
         </div>
     </DefaultContainer>
 </template>
@@ -58,29 +55,55 @@ onMounted(() => {
     margin: 0 auto;
     width: 100%;
     padding: 10px;
+    display:  flex;
+    flex-direction: column;
+    gap: 5px;
 
     h3 {
         margin-top: 10px;
     }
 
+    .el-card{
+        --el-card-padding: 3px 5px;
+    }
+
     .story-wrapper {
         margin-top: 1rem;
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        gap: 5px;
+        padding: 10px;
 
-        .text-wrapper {
-            display: flex;
-            flex-direction: column;
-            align-items: flex-start;
-            gap: 5px;
-            padding: 10px;
-
-            .read-more {
-                align-self: flex-end;
-                color: $base-primary-color;
-                cursor: pointer;
-                font-size: 12px;
-                transition: color 0.2s;
-            }
+        h5 {
+            font-weight: bold;
         }
+
+        .read-more {
+            margin-left: auto;
+            color: $base-primary-color;
+            cursor: pointer;
+            font-size: 10px;
+            transition: color 0.2s;
+        }
+
+
+
+        //.text-wrapper {
+        //    display: flex;
+        //    flex-direction: column;
+        //    align-items: flex-start;
+        //    gap: 5px;
+        //    padding: 10px;
+        //
+        //    .read-more {
+        //        align-self: flex-end;
+        //        color: $base-primary-color;
+        //        cursor: pointer;
+        //        font-size: 12px;
+        //        transition: color 0.2s;
+        //    }
+        //}
     }
 }
 
