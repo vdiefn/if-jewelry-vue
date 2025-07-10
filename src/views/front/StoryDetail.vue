6b835ac886781de72d4efb9d4c5e2dc348912e8e
ellen.kuo <ellen.kuo@patrioimemory.com>
2025-07-10 17:03:05 +0800

    feat(StoryDetail): Add getType function

--- src/views/front/StoryDetail.vue
+++ src/views/front/StoryDetail.vue
@@ -3,11 +3,16 @@ import { DefaultContainer } from "@/components/front/index.js";
 import { useRoute } from "vue-router"
 import { ref, onMounted, watch } from "vue";
 import { reqArticle } from "@/api/front/article.js"
-import { ElBreadcrumb, ElBreadcrumbItem, ElBacktop } from "element-plus";
+import { ElBreadcrumb, ElBreadcrumbItem, ElBacktop, ElTag } from "element-plus";
 
 const route = useRoute()
 const data = ref()
 const loading = ref(false)
+
+const getType = (index) => {
+    const type = ["primary", "success", "info", "warning", "danger"]
+    return type[index%5]
+}
 
 const getArticle = async() => {
     loading.value = true
@@ -45,12 +50,19 @@ onMounted(() => {
             <img :src="data?.image" alt="story picture" />
         </div>
         <div class="text-wrapper">
-            <h4>{{data?.title}}</h4>
+            <div class="title-wrapper">
+                <h4>{{data?.title}}</h4>
+                <div class="tag-wrapper">
+                    <ElTag v-for="(item, index) in data?.tag" :key="index" effect="plain" round :type="getType(index)">
+                        {{ item }}
+                    </ElTag>
+                </div>
+            </div>
             <h6>{{data?.description}}</h6>
             <p>{{data?.content}}</p>
         </div>
     </div>
-    <ElBacktop :right="100" :bottom="100" />
+    <ElBacktop :right="20" :bottom="70" />
 
 </DefaultContainer>
 </template>
@@ -71,10 +83,32 @@ onMounted(() => {
         display: flex;
         flex-direction: column;
         align-items: flex-start;
+        white-space: pre-line;
+        word-break: break-word;
         gap: 10px;
         margin: 1rem;
         padding: 1rem;
+
+        .title-wrapper {
+            width: 100%;
+            display: flex;
+            flex-direction: column;
+            gap: 5px;
+            margin-bottom: 10px;
+
+            .tag-wrapper{
+                align-self: flex-end;
+                display: flex;
+                gap: 3px;
+            }
+        }
+
+        h6 {
+            width: 100%;
+        }
     }
+
+
 }
 
 @media(min-width: $breakpoint-tablet){
@@ -89,6 +123,19 @@ onMounted(() => {
     .story-wrapper {
         width: 650px;
         margin: 0 auto;
+
+        .title-wrapper {
+
+            h4 {
+                flex: 0 1 auto;
+                font-weight: bold;
+            }
+
+            .tag-wrapper {
+                display: flex;
+                gap: 5px;
+            }
+        }
 
         .img-wrapper {
             padding: 1rem;
