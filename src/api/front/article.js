54462ffc1c0bab109163abeda90c1e91cb7f9cd9
ellen.kuo <ellen.kuo@patrioimemory.com>
2025-12-15 11:00:16 +0800

    feat: add types of article API

--- src/api/front/article.js
+++
@@ -1,9 +0,0 @@
-import api from "@/utils/api.js";
-
-export const reqAllArticles = () => {
-    return api.get("/articles")
-}
-
-export const reqArticle = (id) => {
-    return api.get(`/article/${id}`)
-}
