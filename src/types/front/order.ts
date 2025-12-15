0ee2807695a3306b20f53c10beae892c12381f4e
ellen.kuo <ellen.kuo@patrioimemory.com>
2025-12-15 10:51:40 +0800

    feat: update types of order

--- src/types/front/order.ts
+++ src/types/front/order.ts
@@ -1,3 +1,5 @@
+import type { CouponData } from "./cart"
+
 export interface UserData {
     name: string,
     email: string,
@@ -18,4 +20,50 @@ export interface CreateOrderResponse {
     total: number,
     create_at: number,
     orderId: string
+}
+
+export type OrderProduct = Record<string, OrderProductItem>
+
+export interface CouponProductData extends CouponData {
+    num: number
+}
+
+export interface ProductData {
+    category: string,
+    content:string,
+    description: string,
+    id: string,
+    imageUrl: string,
+    is_enabled: number,
+    num: number,
+    origin_price: number,
+    price: number,
+    title: string,
+    unit: string
+}
+
+export interface OrderProductItem {
+    coupon?: CouponProductData,
+    final_total: number,
+    id: string,
+    product: ProductData
+    product_id:string,
+    qty:number,
+    total:number
+}
+
+export interface OrderData {
+    create_at: number,
+    id: string,
+    is_paid: boolean,
+    message: string,
+    paid_date: number,
+    products: OrderProduct,
+    total: number,
+    user: UserData
+}
+
+export interface GetOrderResponse {
+    success: boolean,
+    order: OrderData,
 }
