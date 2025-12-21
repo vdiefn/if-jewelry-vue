25c745dfcc3274252a47039753508d4d216debf2
Ellen <ddhkxxhxx@hotmail.com>
2025-12-21 15:18:29 +0800

    feat: add update article page with ts setting

--- src/types/front/article.ts
+++ src/types/front/article.ts
@@ -1,29 +1,27 @@
-import type { PaginationEmits } from "element-plus"
-import type { Pagination } from "./product"
+import type { PaginationEmits } from "element-plus";
+import type { Pagination } from "./product";
 
 export interface ArticleData {
-    title:string,
-    description:string,
-    image: string,
-    tag: string[],
-    create_at: number,
-    author: string,
-    isPublic: boolean,
-    content: string
+  title: string;
+  description: string;
+  image: string;
+  tag: string[];
+  create_at: number;
+  author: string;
+  isPublic: boolean;
+  content: string;
 }
 
 export interface GetArticleResponse {
-    success: boolean,
-    articles: ArticleData[],
-    pagination: Pagination
-    messages: string[]
+  success: boolean;
+  articles: ArticleData[];
+  pagination: Pagination;
+  messages: string[];
 }
 
 export interface GetArticleDetailResponse {
-    success: boolean,
-    articles: ArticleData,
-    pagination: Pagination,
-    messages: string[]
+  success: boolean;
+  article: ArticleData;
+  pagination: Pagination;
+  messages: string[];
 }
-
-
