736bd260f8538b081d9014cd6dd15324b0208d04
ellen.kuo <ellen.kuo@patrioimemory.com>
2025-12-29 16:37:50 +0800

    feat: update type setting

--- src/layout/admin/MenuList.vue
+++ src/layout/admin/MenuList.vue
@@ -1,55 +1,53 @@
 <script setup lang="ts">
 import { useRouter } from "vue-router";
-import type { RouteRecordRaw } from "vue-router"
+import type { RouteRecordRaw } from "vue-router";
 
 const router = useRouter();
 
-const props = defineProps({
-    menuList: {
-        type: Object,
-    },
-});
+const props = defineProps<{
+  menuList: RouteRecordRaw[];
+}>();
 
-const goRoute = (item:RouteRecordRaw) => {
-    router.push(item.path);
+const goRoute = (item: RouteRecordRaw) => {
+  router.push(item.path);
 };
 </script>
 
 <template>
-    <template v-for="(item, index) in menuList" :key="item.path">
-        <template v-if="item.children">
-        <MenuList :menuList="item.children"></MenuList>
-        </template>
-        <template v-else>
-        <ElMenuItem
-            :index="item.path"
-            @click="goRoute(item)"
-            v-if="!item.meta.hidden"
-        >
-            <ElIcon>
-            <component :is="item.meta.icon"></component>
-            </ElIcon>
-            <template #title>{{ item.meta.title }}</template>
-        </ElMenuItem>
-        </template>
+  <template v-for="(item, index) in menuList" :key="item.path">
+    <template v-if="item.children">
+      <MenuList :menuList="item.children"></MenuList>
     </template>
+    <template v-else>
+      <ElMenuItem
+        :index="item.path"
+        @click="goRoute(item)"
+        v-if="!item.meta?.hidden"
+      >
+        <ElIcon>
+          <component :is="item.meta?.icon"></component>
+        </ElIcon>
+        <template #title>{{ item.meta?.title }}</template>
+      </ElMenuItem>
+    </template>
+  </template>
 </template>
 
 <style scoped lang="scss">
 .el-menu-item {
-    transition: transform 0.3s ease-in-out;
+  transition: transform 0.3s ease-in-out;
 }
 
 .el-menu-item:hover {
-    background: $base-background-color;
-    border-top-left-radius: 30px;
-    border-bottom-left-radius: 30px;
-    transform: translateX(20%);
+  background: $base-background-color;
+  border-top-left-radius: 30px;
+  border-bottom-left-radius: 30px;
+  transform: translateX(20%);
 }
 
 .el-menu-item.is-active {
-    color: rgb(34, 33, 33);
-    font-weight: 900;
-    font-size: 0.95rem;
+  color: rgb(34, 33, 33);
+  font-weight: 900;
+  font-size: 0.95rem;
 }
 </style>
