a29ca69fabb9b145a61774b1118c261fdffa4a3e
ellen.kuo <ellen.kuo@patrioimemory.com>
2025-12-29 17:10:26 +0800

    feat: update indent

--- src/components/front/DefaultContainer.vue
+++ src/components/front/DefaultContainer.vue
@@ -1,16 +1,13 @@
-<script setup>
-import { ref } from 'vue';
-
-</script>
+<script setup></script>
 
 <template>
-    <div class="container">
-        <slot></slot>
-    </div>
+  <div class="container">
+    <slot></slot>
+  </div>
 </template>
 
 <style scoped lang="scss">
 .container {
-    margin-top: $base-header-height;
+  margin-top: $base-header-height;
 }
 </style>
--- src/components/front/Footer.vue
+++ src/components/front/Footer.vue
@@ -1,78 +1,75 @@
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
-    <div class="container">
-        <div class="about-us footer-item">
-            <h4>About Us</h4>
-            <RouterLink :to="{name: 'About'}"><p>品牌故事</p></RouterLink>
-            <RouterLink :to="{name: 'JewelryStory'}"><p>寶石小教室</p></RouterLink>
-        </div>
-        <div class="service footer-item">
-            <h4>Service</h4>
-            <RouterLink :to="{name: 'Faq'}"><p>常見問題</p></RouterLink>
-            <RouterLink :to="{name: 'ShippingAndPayment'}"><p>運送及付款方式</p></RouterLink>
-        </div>
-        <div class="contact-us footer-item">
-            <h4>Contact Us</h4>
-            <p>If Jewelry 如果工作室</p>
-            <p>客服電話: 02-8765-4321</p>
-            <p>客服信箱: support@if-jewelry.com</p>
-            <p>服務時間: 週一~週五 09:00 - 18:30</p>
-        </div>
-        <div class="follow-us footer-item">
-            <h4>Follow Us</h4>
-            <div class="icon">
-                <i>
-                    <font-awesome-icon :icon="['fab', 'facebook']" />
-                </i>
-                <i>
-                    <font-awesome-icon :icon="['fab', 'instagram']" />
-                </i>
-                <i>
-                    <font-awesome-icon :icon="['fab', 'line']" />
-                </i>
-            </div>
-        </div>
-
-
+  <div class="container">
+    <div class="about-us footer-item">
+      <h4>About Us</h4>
+      <RouterLink :to="{ name: 'About' }"><p>品牌故事</p></RouterLink>
+      <RouterLink :to="{ name: 'JewelryStory' }"><p>寶石小教室</p></RouterLink>
     </div>
+    <div class="service footer-item">
+      <h4>Service</h4>
+      <RouterLink :to="{ name: 'Faq' }"><p>常見問題</p></RouterLink>
+      <RouterLink :to="{ name: 'ShippingAndPayment' }"
+        ><p>運送及付款方式</p></RouterLink
+      >
+    </div>
+    <div class="contact-us footer-item">
+      <h4>Contact Us</h4>
+      <p>If Jewelry 如果工作室</p>
+      <p>客服電話: 02-8765-4321</p>
+      <p>客服信箱: support@if-jewelry.com</p>
+      <p>服務時間: 週一~週五 09:00 - 18:30</p>
+    </div>
+    <div class="follow-us footer-item">
+      <h4>Follow Us</h4>
+      <div class="icon">
+        <i>
+          <font-awesome-icon :icon="['fab', 'facebook']" />
+        </i>
+        <i>
+          <font-awesome-icon :icon="['fab', 'instagram']" />
+        </i>
+        <i>
+          <font-awesome-icon :icon="['fab', 'line']" />
+        </i>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped lang="scss">
 .container {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+
+  .footer-item {
+    > p {
+      margin-top: 3px;
+    }
+  }
+
+  .icon {
     display: flex;
-    flex-direction: column;
-    gap: 1rem;
-
-    .footer-item {
-        > p{
-            margin-top: 3px
-        }
-    }
-
-    .icon {
-        display: flex;
-        gap:10px;
-    }
+    gap: 10px;
+  }
 }
 
-
 @media (min-width: $breakpoint-tablet) {
-    .container {
-        max-width: 1280px;
-        flex-direction: row;
-        gap: 10px;
-        justify-content: space-between;
-        margin: 0 auto;
-        padding: 1rem 1rem;
+  .container {
+    max-width: 1280px;
+    flex-direction: row;
+    gap: 10px;
+    justify-content: space-between;
+    margin: 0 auto;
+    padding: 1rem 1rem;
 
-        .follow-us {
-            .icon {
-                justify-content: center;
-            }
-        }
+    .follow-us {
+      .icon {
+        justify-content: center;
+      }
     }
+  }
 }
 </style>
