fd3ec8ca4cc789a918db53465373c7f004cd649a
ellen.kuo <ellen.kuo@patrioimemory.com>
2026-01-02 11:33:20 +0800

    feat: add ContentContainer

---
+++ src/components/front/ContentContainer.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+
+</script>
+<template>
+<div class="content">
+    <slot />
+  </div>
+</template>
+<style lang="scss" scoped>
+.content {
+  margin: 0 auto;
+}
+
+@media (min-width: $breakpoint-tablet) {
+  .content {
+    max-width: 650px;
+  }
+}
+
+@media (min-width: $breakpoint-desktop) {
+  .content {
+    max-width: 900px;
+  }
+}
+</style>
